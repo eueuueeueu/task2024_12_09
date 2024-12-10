0df7a4a444345128f3e092fac72fc48ef5886025
DongHao <3364836519@qq.com>
2024-12-10 09:38:33 +0800

    1

--- js/01.js
+++ js/01.js
@@ -1,28 +1,38 @@
 let data = [
-  { name: '魏璎珞', subject: 'JavaScript', grade: 100 },
-  { name: '弘历', subject: 'JavaScript', grade: 90 },
-  { name: '傅恒', subject: 'JavaScript', grade: 99 },
-  { name: '明玉', subject: 'JavaScript', grade: 89 }
+  { id: 1, name: '魏璎珞', subject: 'JavaScript', grade: 100 },
+  { id: 2, name: '弘历', subject: 'JavaScript', grade: 90 },
+  { id: 3, name: '傅恒', subject: 'JavaScript', grade: 99 },
+  { id: 4, name: '明玉', subject: 'JavaScript', grade: 89 }
 ]
+let keys = ['name', 'subject', 'grade', 'action']
 function addTbody(data) {
   let tbody = document.querySelector('tbody')
   for (let i = 0; i < data.length; i++) {
     let tr = document.createElement('tr')
-    for (const key in data[i]) {
+    // for (const key in data[i]) {
+    //   let td = document.createElement('td')
+    //   td.innerHTML = data[i][key]
+    //   tr.appendChild(td)
+    // }
+    for (let j = 0; j < keys.length; j++) {
       let td = document.createElement('td')
-      td.innerHTML = data[i][key]
+      let tdValue = data[i][keys[j]] !== undefined ? data[i][keys[j]] : `<a class="action" href="#">删除</a>`
+      td.innerHTML = tdValue
       tr.appendChild(td)
     }
-    let td = document.createElement('td')
-    td.innerHTML = `<a href="#">删除</a>`
-    tr.appendChild(td)
     tbody.appendChild(tr)
   }
-  let allA = document.querySelectorAll('a')
-  for (let j = 0; j < allA.length; j++) {
-    allA[j].addEventListener('click', function (e) {
-      tbody.removeChild(this.parentNode.parentNode);
-    })
-  }
+  // let allA = document.querySelectorAll('a')
+  // for (let j = 0; j < allA.length; j++) {
+  //   allA[j].addEventListener('click', function (e) {
+  //     tbody.removeChild(this.parentNode.parentNode);
+  //   })
+  // }
+  tbody.addEventListener('click', e => {
+    e.stopPropagation()
+    if (e.target.className = 'action') {
+      e.target.parentNode.parentNode.remove()
+    }
+  })
 }
 addTbody(data)
